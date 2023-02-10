555ed6b7568eba7337793784e9c9cccb78d5b582
praveena <cheerful.praveena@gmail.com>
2023-02-10 17:11:05 +0000

    JIRA 1000 - Trying Jenkins

---
+++ src/test/java/uk/axone/devintest/maventests/Application_Tests.java
@@ -0,0 +1,19 @@
+package uk.axone.devintest.maventests;
+
+import org.testng.annotations.Test;
+
+public class Application_Tests {
+    @Test
+    public void test1() {
+        System.out.println("running test1");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("running test2");
+    }
+    @Test
+    public void test3() {
+        System.out.println("trying Jenkins");
+    }
+}
